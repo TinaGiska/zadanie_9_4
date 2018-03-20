ded11c4e856c7d23df48c18944315b8a004df812
Tina Giska <tina.tylicka@gmail.com>
2018-03-20 13:51:04 +0100

    Update js

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,21 @@
-var x = 10 ;
-
-
+function drawTree(x) {
 
 for ( var i = 1 ; i <= x ; i++) {
 	var star = '';
 
-     for(j=i; j<x; j++) {
-            star += ' ';
+for(j=i; j<x; j++) {
+        star += ' ';
 }
 
 for ( var j = 1 ; j <= (2*i-1) ; j++) {
-	star += '*';
+		star += '*';
 }
 console.log(star);
-	}
 
+	}
+}
+
+drawTree(10)
 
 
 
