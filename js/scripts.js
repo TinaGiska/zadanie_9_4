c7ef3e01207a4eddc47c66c04b52dfe8e88971e6
Tina Giska <tina.tylicka@gmail.com>
2018-03-20 13:15:25 +0100

    Update scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+var x = 10 ;
+
+
+
+for ( var i = 1 ; i <= x ; i++) {
+	var star = '';
+
+     for(j=i; j<x; j++) {
+            star += ' ';
+}
+
+for ( var j = 1 ; j <= (2*i-1) ; j++) {
+	star += '*';
+}
+console.log(star);
+	}
+
+
+
+
